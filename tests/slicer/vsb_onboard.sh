b558ca35b88218bc2d7ec9ab4ca7937db7bccf52
Avi Weit <weit@il.ibm.com>
2021-02-07 21:40:09 +0200

    Initial commit of vsb on-board helper script.

---
+++ tests/slicer/vsb_onboard.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export SLICER="localhost"
+export BETWEEN=3
+
+echo ""
+echo ""
+echo "-=-=-=-=-=-= TRACE -=-=-=-=-=-=-=-=-=-"
+echo "Login admin.."
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-"
+
+curl -i -X POST -d username=admin -d password=admin -c admin_credentials http://$SLICER:8082/login 2>/dev/null
+
+sleep $BETWEEN
+echo ""
+echo ""
+echo "-=-=-=-=-=-= TRACE -=-=-=-=-=-=-=-=-=-"
+echo "Create blueprint"
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-"
+
+curl -b admin_credentials -d @vsb_VideoStreaming.json -X POST http://$SLICER:8082/vs/catalogue/vsblueprint --header "Content-Type:application/json"
