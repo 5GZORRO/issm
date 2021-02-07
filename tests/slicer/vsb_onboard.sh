9938b71be09051fa056d68f8eeefc15045e4257c
Avi Weit <weit@il.ibm.com>
2021-02-07 22:58:35 +0200

    Fix EOL.

--- tests/slicer/vsb_onboard.sh
+++ tests/slicer/vsb_onboard.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# Helper script to onboared a given blueprint.
+#
+
 export SLICER="localhost"
 export BETWEEN=3
 
@@ -9,7 +13,7 @@ echo "-=-=-=-=-=-= TRACE -=-=-=-=-=-=-=-=-=-"
 echo "Login admin.."
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-"
 
-curl -i -X POST -d username=admin -d password=admin -c admin_credentials http://$SLICER:8082/login 2>/dev/null
+curl -i -X POST -d username=admin -d password=admin -c ./admin_credentials http://$SLICER:8082/login 2>/dev/null
 
 sleep $BETWEEN
 echo ""
@@ -18,4 +22,4 @@ echo "-=-=-=-=-=-= TRACE -=-=-=-=-=-=-=-=-=-"
 echo "Create blueprint"
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-"
 
-curl -b admin_credentials -d @vsb_VideoStreaming.json -X POST http://$SLICER:8082/vs/catalogue/vsblueprint --header "Content-Type:application/json"
+curl -b ./admin_credentials -d @vsb_VideoStreaming.json -X POST http://$SLICER:8082/vs/catalogue/vsblueprint --header "Content-Type:application/json"
