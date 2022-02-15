0e4412d105193c742f7a18d66d64386097bb0312
Avi Weit <weit@il.ibm.com>
2022-02-15 12:03:21 +0200

    Add 'Access-Control-Allow-Headers'.

--- api/server.py
+++ api/server.py
@@ -324,7 +324,7 @@ def getMessagePayload():
 @proxy.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    #response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,POST,DELETE')
     return response
 
