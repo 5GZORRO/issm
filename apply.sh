c0d2c432faeb396107057c071471eaa3cf3c6348
Avi Weit <weit@il.ibm.com>
2022-02-20 10:50:27 +0200

    Update apply.sh.

--- apply.sh
+++ apply.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ORCH=$1
 
+kubectl apply -f $SCRIPT_DIR/flows/issm-sensor.yaml -n $MNO_NAMESPACE
+
 kubectl apply -f $SCRIPT_DIR/wf-templates/base.yaml -n $MNO_NAMESPACE
 kubectl apply -f $SCRIPT_DIR/scenarios/1/scenario-1-submit.yaml -n $MNO_NAMESPACE
 kubectl apply -f $SCRIPT_DIR/scenarios/2/scenario-2-submit.yaml -n $MNO_NAMESPACE
