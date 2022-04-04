a6c074722dc19c1b26f51a2b56f238dce9aadbff
Avi Weit <weit@il.ibm.com>
2022-04-04 13:31:16 +0300

    Rename scenario 2 to "uc3".

--- apply-sla.sh
+++ apply-sla.sh
@@ -7,4 +7,4 @@ kubectl apply -f $SCRIPT_DIR/flows/issm-sla-breach-sensor.yaml -n issm
 
 kubectl apply -f $SCRIPT_DIR/wf-templates/base.yaml -n issm
 kubectl apply -f $SCRIPT_DIR/scenarios/1/scenario-1-breach.yaml -n issm
-kubectl apply -f $SCRIPT_DIR/scenarios/2/scenario-2-breach.yaml -n issm
+kubectl apply -f $SCRIPT_DIR/scenarios/uc3/uc3-breach.yaml -n issm
--- apply.sh
+++ apply.sh
@@ -8,14 +8,14 @@ kubectl apply -f $SCRIPT_DIR/flows/issm-sensor.yaml -n $MNO_NAMESPACE
 
 kubectl apply -f $SCRIPT_DIR/wf-templates/base.yaml -n $MNO_NAMESPACE
 kubectl apply -f $SCRIPT_DIR/scenarios/1/scenario-1-submit.yaml -n $MNO_NAMESPACE
-kubectl apply -f $SCRIPT_DIR/scenarios/2/scenario-2-submit.yaml -n $MNO_NAMESPACE
+kubectl apply -f $SCRIPT_DIR/scenarios/uc3/uc3-submit.yaml -n $MNO_NAMESPACE
 kubectl apply -f $SCRIPT_DIR/scenarios/3/scenario-3-submit.yaml -n $MNO_NAMESPACE
 
 if [ "$ORCH" = "NSSO" ]; then
     kubectl apply -f $SCRIPT_DIR/scenarios/1/orch-nsso.yaml -n $MNO_NAMESPACE
-    kubectl apply -f $SCRIPT_DIR/scenarios/2/orch-nsso.yaml -n $MNO_NAMESPACE
+    kubectl apply -f $SCRIPT_DIR/scenarios/uc3/orch-nsso.yaml -n $MNO_NAMESPACE
 else
     kubectl apply -f $SCRIPT_DIR/scenarios/1/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
-    kubectl apply -f $SCRIPT_DIR/scenarios/2/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
+    kubectl apply -f $SCRIPT_DIR/scenarios/uc3/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
     kubectl apply -f $SCRIPT_DIR/scenarios/3/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
 fi
