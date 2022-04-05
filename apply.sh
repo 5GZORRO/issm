f963c0fbaecceb1cb6fd8dbddf08868e45d94d55
Avi Weit <weit@il.ibm.com>
2022-04-05 11:12:22 +0300

    Fix template not found error.

--- apply.sh
+++ apply.sh
@@ -4,6 +4,14 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ORCH=$1
 
+echo ""
+echo ""
+echo "-=-=-=-=-=-= TRACE -=-=-=-=-=-=-=-=-=-"
+echo "Applying templates for domain: $MNO_NAMESPACE"
+echo "Orchestrator type: $1"
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-"
+
+
 kubectl  delete  workflowtemplate --all -n  $MNO_NAMESPACE
 
 kubectl apply -f $SCRIPT_DIR/flows/issm-sensor.yaml -n $MNO_NAMESPACE
@@ -15,7 +23,13 @@ kubectl apply -f $SCRIPT_DIR/scenarios/uc1.2/uc1.2-submit.yaml -n $MNO_NAMESPACE
 
 if [ "$ORCH" = "NSSO" ]; then
     kubectl apply -f $SCRIPT_DIR/scenarios/1/orch-nsso.yaml -n $MNO_NAMESPACE
+    kubectl apply -f $SCRIPT_DIR/scenarios/uc1.2/orch-nsso.yaml -n $MNO_NAMESPACE
     kubectl apply -f $SCRIPT_DIR/scenarios/uc3/orch-nsso.yaml -n $MNO_NAMESPACE
-else
+elif [ "$ORCH" = "MEC" ]; then
+    kubectl apply -f $SCRIPT_DIR/scenarios/1/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
     kubectl apply -f $SCRIPT_DIR/scenarios/uc1.2/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
+    kubectl apply -f $SCRIPT_DIR/scenarios/uc3/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
+else
+    echo "Illegal Orchestrator type value: [$1]"
+    exit 1
 fi
