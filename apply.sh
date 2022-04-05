cdb9ae0f28946146ada8b39d10ba791a560b6304
Avi Weit <weit@il.ibm.com>
2022-04-05 10:03:21 +0300

    Add scenario for handling UC1.2.

--- apply.sh
+++ apply.sh
@@ -4,18 +4,18 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ORCH=$1
 
+kubectl  delete  workflowtemplate --all -n  $MNO_NAMESPACE
+
 kubectl apply -f $SCRIPT_DIR/flows/issm-sensor.yaml -n $MNO_NAMESPACE
 
 kubectl apply -f $SCRIPT_DIR/wf-templates/base.yaml -n $MNO_NAMESPACE
 kubectl apply -f $SCRIPT_DIR/scenarios/1/scenario-1-submit.yaml -n $MNO_NAMESPACE
 kubectl apply -f $SCRIPT_DIR/scenarios/uc3/uc3-submit.yaml -n $MNO_NAMESPACE
-kubectl apply -f $SCRIPT_DIR/scenarios/3/scenario-3-submit.yaml -n $MNO_NAMESPACE
+kubectl apply -f $SCRIPT_DIR/scenarios/uc1.2/uc1.2-submit.yaml -n $MNO_NAMESPACE
 
 if [ "$ORCH" = "NSSO" ]; then
     kubectl apply -f $SCRIPT_DIR/scenarios/1/orch-nsso.yaml -n $MNO_NAMESPACE
     kubectl apply -f $SCRIPT_DIR/scenarios/uc3/orch-nsso.yaml -n $MNO_NAMESPACE
 else
-    kubectl apply -f $SCRIPT_DIR/scenarios/1/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
-    kubectl apply -f $SCRIPT_DIR/scenarios/uc3/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
-    kubectl apply -f $SCRIPT_DIR/scenarios/3/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
+    kubectl apply -f $SCRIPT_DIR/scenarios/uc1.2/orch-mec-cnmp.yaml -n $MNO_NAMESPACE
 fi
