5f4b3bb86552b99fdc2d01a1342e93e9a175c9ba
Avi Weit <weit@il.ibm.com>
2022-05-22 12:39:52 +0300

    Add snfvo Network Service scaleout.

--- apply-sla.sh
+++ apply-sla.sh
@@ -3,8 +3,10 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 
-kubectl apply -f $SCRIPT_DIR/flows/issm-sla-breach-sensor.yaml -n issm
+kubectl apply -f $SCRIPT_DIR/flows/issm-sla-breach-sensor-v2.yaml -n issm
+
+kubectl delete workflowtemplates --all -n issm
 
 kubectl apply -f $SCRIPT_DIR/wf-templates/base.yaml -n issm
-kubectl apply -f $SCRIPT_DIR/scenarios/1/scenario-1-breach.yaml -n issm
-kubectl apply -f $SCRIPT_DIR/scenarios/uc3/uc3-breach.yaml -n issm
+kubectl apply -f $SCRIPT_DIR/wf-templates/breach-v2.yaml -n issm
+kubectl apply -f $SCRIPT_DIR/wf-templates/transaction-v2.yaml -n issm
