ff28cd06411e13ecc5bbdd8b2d6b08464eec76b8
Avi Weit <weit@il.ibm.com>
2022-08-01 15:54:58 +0300

    Add sensor that listens on notifications related to its domain.
    - sensor to process notifications (e.g. offer created/deleted,
      order created/deleted, ...)

--- apply-domain.sh
+++ apply-domain.sh
@@ -7,16 +7,31 @@ ORCH=$1
 echo ""
 echo ""
 echo "-=-=-=-=-=-= TRACE -=-=-=-=-=-=-=-=-=-"
-echo "Applying templates for domain: $MNO_NAMESPACE"
+echo "Applying templates for domain: $MNO_NAME"
 echo "Orchestrator type: $1"
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-"
 
+MNO_NAMESPACE=domain-$MNO_NAME
 
+kubectl  delete  es --all -n  $MNO_NAMESPACE
 kubectl  delete  workflowtemplate --all -n  $MNO_NAMESPACE
 kubectl  delete  sensor --all -n  $MNO_NAMESPACE
 
+KAFKA_HOST=172.28.3.196
+KAFKA_PORT=9092
+
+ISSM_DOMAIN_TOPIC=issm-in-$MNO_NAME
+envsubst < $SCRIPT_DIR/deploy/kafka-event-source.yaml.template | kubectl apply -n $MNO_NAMESPACE -f -
+
+SLA_BREACH_DOMAIN_TOPIC=issm-breach-$MNO_NAME
+envsubst < $SCRIPT_DIR/deploy/kafka-domain-sla-breach-event-source.yaml.template | kubectl apply -n $MNO_NAMESPACE -f -
+
+AUX_DOMAIN_TOPIC=issm-aux-$MNO_NAME
+envsubst < $SCRIPT_DIR/deploy/kafka-domain-aux-event-source.yaml.template | kubectl apply -n $MNO_NAMESPACE -f -
+
 kubectl apply -f $SCRIPT_DIR/sensors/issm-domain-sensor-v2.yaml -n $MNO_NAMESPACE
 kubectl apply -f $SCRIPT_DIR/sensors/issm-domain-sla-breach-sensor-v2.yaml -n $MNO_NAMESPACE
+kubectl apply -f $SCRIPT_DIR/sensors/issm-domain-aux-sensor-v2.yaml -n $MNO_NAMESPACE
 
 kubectl apply -f $SCRIPT_DIR/wf-templates/ -n $MNO_NAMESPACE
 
