54d66d5b3aeed0e1868f348c3b51c5d5c87a471b
Avi Weit <weit@il.ibm.com>
2022-09-07 14:52:47 +0300

    Add issm-mec-cnmp orchestration template.
    - define vcdn intent
    - write vcdn snfvo that uses ISSM-O

--- snfvo/apply-snfvo.sh
+++ snfvo/apply-snfvo.sh
@@ -8,4 +8,4 @@ python3 ./cli.py --file ./spectrum.yaml --service_owner operator-c --issm_api_ur
 python3 ./cli.py --file ./spectrum-slice.yaml --service_owner operator-b --issm_api_url http://172.28.3.15:30080 --snfvo_name "Slice offer with configurable spectrum" --product_offer_id f5bfa01e-83e5-4c29-9860-1efd36aa189c
 
 python3 ./cli.py --file ./vcdn.yaml --service_owner operator-c --issm_api_url http://172.28.3.15:30080 --snfvo_name "CDN Network Service (CDN+SAS)" --product_offer_id 7ba86cad-b147-4aec-8ece-f0c1b748b9b8
-python3 ./cli.py --file ./optimizer.yaml --service_owner operator-c --issm_api_url http://172.28.3.15:30080 --snfvo_name "Optimized Service (e2e test of ISSM-O)" --product_offer_id 416767e2-cd33-4b62-b2d5-074aaa156004
+python3 ./cli.py --file ./vcdn-mec-cnmp.yaml --service_owner operator-c --issm_api_url http://172.28.3.15:30080 --snfvo_name "vCDN Optimized Service (e2e test of ISSM-O)" --product_offer_id 416767e2-cd33-4b62-b2d5-074aaa156004
