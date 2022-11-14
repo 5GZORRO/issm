3829093984e14a51e7b63c2d1f7a1784e44b0d46
Avi Weit <weit@il.ibm.com>
2022-11-14 17:55:00 +0200

    Add scaleout entry point.

--- snfvo/apply-snfvo.sh
+++ snfvo/apply-snfvo.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-python3 ./cli.py --file ./opt-topology.yaml --service_owner operator-c --issm_api_url http://172.28.3.15:30080 --snfvo_name "OTA Composite Offer" --product_offer_id  673db4c4-c2a4-4348-9e54-0f916f56d5d9
+python3 ./cli.py --file ./free5gc-slice.yaml --service_owner operator-a --issm_api_url http://172.28.3.15:30080 --snfvo_name "free5gc slice snfvo" --product_offer_id  ff3635ff-7f9a-4475-8681-16248b610659
